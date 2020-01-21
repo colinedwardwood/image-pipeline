a6214e65d7d43933da33e923fd30443296aca967
cewdesign <woodce@gmail.com>
2020-01-21 14:40:11 -0500

    removed habitat control & insert placeholder test

--- inspec/baseline/controls/habitat.rb
+++ inspec/baseline/controls/habitat.rb
@@ -16,14 +16,20 @@
 # This control should check to see if habitat is installed and running on target system.
 # 
 
-control 'habitat' do
-  impact 1.0
-  title 'shrug'
-  desc 'shrug description'
-  describe habitat_service(origin: 'core', name: 'httpd') do
-    it                     { should exist }
-    # its('version')         { should eq '2.4.35'}
-    # its('topology')        { should eq 'standalone' }
-    # its('update_strategy') { should eq 'none' }
+# control 'habitat' do
+#   impact 1.0
+#   title 'shrug'
+#   desc 'shrug description'
+#   describe habitat_service(origin: 'core', name: 'httpd') do
+#     it                     { should exist }
+#     # its('version')         { should eq '2.4.35'}
+#     # its('topology')        { should eq 'standalone' }
+#     # its('update_strategy') { should eq 'none' }
+#   end
+# end
+
+control 'my-test-control' do
+  describe package ('glibc') do
+    it { should be_installed }
   end
 end
